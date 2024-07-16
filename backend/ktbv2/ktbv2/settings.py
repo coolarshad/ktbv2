bb8d5753ad2b7d18f9a68be4f2ee8dcfc5d7ac26
Arshad Qadir <coolarshad@Arshads-Mini.domain.name>
2024-07-16 17:11:03 +0545

    using vite for react

--- backend/ktbv2/ktbv2/settings.py
+++ backend/ktbv2/ktbv2/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'trademgt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +54,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 ROOT_URLCONF = 'ktbv2.urls'
 
 TEMPLATES = [
