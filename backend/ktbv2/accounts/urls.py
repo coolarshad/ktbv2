ba3ef8f07fbec5d42bcfae9d006cb48101d53370
saiyad <saiyad@fetch.ly>
2026-04-06 23:40:54 +0545

    url added

--- backend/ktbv2/accounts/urls.py
+++ backend/ktbv2/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserListCreateView, UserRetrieveUpdateDestroyView
+from .views import PermissionListCreateView, PermissionRetrieveUpdateDestroyView, UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('users/', UserListCreateView.as_view()),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view()),
+
+     # Permissions
+    path('permissions/', PermissionListCreateView.as_view(), name='permission-list-create'),
+    path('permissions/<int:pk>/', PermissionRetrieveUpdateDestroyView.as_view(), name='permission-detail'),
+
 ]
