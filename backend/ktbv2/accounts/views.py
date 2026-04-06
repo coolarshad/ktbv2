5a8308d1657ff01328be9b2fe61fa32d4d07a06a
saiyad <saiyad@fetch.ly>
2026-04-06 23:46:25 +0545

    import module fixed

--- backend/ktbv2/accounts/views.py
+++ backend/ktbv2/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
 # Create your views here.
 from rest_framework import generics
 from accounts.models import CustomUser, Permission
